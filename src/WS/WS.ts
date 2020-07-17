c0be099a852fe440e28401243215e1825ec61af7
James (smitteh1) <thejamesash1@gmail.com>
2020-07-17 00:29:12 -0400

    add express defaults.

--- src/WS/WS.ts
+++ src/WS/WS.ts
@@ -1,11 +1,3 @@
 /// <refrence path="../util/@types/global.d.ts" />
-import express from "express";
-import morgan from "morgan";
-import session from "express-session";
-import config from "../config";
-
-const app = express();
-
-
 
 export default null;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,22 @@
 /// <refrence path="../util/@types/global.d.ts" />
 
+import * as https from "https";
+import * as fs from "fs";
+
 const config = {
+
+    web: {
+		userAgent: `Octogo.nl`,
+		host: "localhost",
+		secure: true,
+		get port() { return 0 || (config.web.secure ? 443 : 80); },
+		serverOptions: {
+			ca: fs.readFileSync(`${__dirname}/ssl/CA-Chain-X1.crt`).toString(),
+			cert: fs.readFileSync(`${__dirname}/ssl/plunge.gg.crt`).toString(),
+			key: fs.readFileSync(`${__dirname}/ssl/plunge.gg.key`).toString()
+		} as https.ServerOptions,
+		cookieSecret: "ZTXTfjbMVdqWryvaJz9s9mT2Q3zbxAUeUGU7mwMX"
+	}
 
 };
 
