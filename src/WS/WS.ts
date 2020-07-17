2076c5e2cc68e84a75f6d830cfa141396ff3af04
James (smitteh1) <thejamesash1@gmail.com>
2020-07-17 00:12:54 -0400

    add more deps

--- src/WS/WS.ts
+++ src/WS/WS.ts
@@ -1,6 +1,10 @@
 /// <refrence path="../util/@types/global.d.ts" />
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
+import config from "../config";
+
+const app = express();
 
 
 
